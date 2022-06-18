ee722233744bfaf8fb33ef72eefa56bb786cbc9c
Nikita Dubyk <nikita_dubyk@mail.ru>
2022-06-18 18:01:44 +0300

    added lazy import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import { lazy, Suspense } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import { useAuth } from './hook/auth.hook'
 
 import Header from './components/Header'
 import Home from './pages/Home'
-import Market from './pages/Market'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import Orders from './pages/Orders'
+import Spinner from './components/Spinner'
+
+const Market = lazy(() => import('./pages/Market'))
+const Cart = lazy(() => import('./pages/Cart'))
+const Login = lazy(() => import('./pages/Login'))
+const Orders = lazy(() => import('./pages/Orders'))
 
 function App() {
     const { token } = useAuth()
@@ -15,14 +17,18 @@ function App() {
     return (
         <>
             <Header />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/market/:id' element={<Market />} />
-                <Route path='/cart' element={<Cart />} />
-                <Route path='/login' element={<Login />} />
-                {token && <Route path='/orders' element={<Orders />} />}
-                <Route path='*' element={<Navigate to='/' replace />} />
-            </Routes>
+            <main>
+                <Suspense fallback={<Spinner />}>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/market/:id' element={<Market />} />
+                        <Route path='/cart' element={<Cart />} />
+                        <Route path='/login' element={<Login />} />
+                        {token && <Route path='/orders' element={<Orders />} />}
+                        <Route path='*' element={<Navigate to='/' replace />} />
+                    </Routes>
+                </Suspense>
+            </main>
         </>
     )
 }
